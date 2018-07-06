d854dd402df649973091cf1ae6c0c5a4418a6337
mac <1540086109@qq.com>
2018-07-06 10:39:10 +0800

    no message

--- BaiduMapDemo /BaiduMapDemo/AppDelegate.m
+++ BaiduMapDemo /BaiduMapDemo/AppDelegate.m
@@ -5,6 +5,7 @@
 //  Created by xyj on 2018/5/10.
 //  Copyright © 2018年 xyj. All rights reserved.
 
+
 #import <BaiduMapAPI_Base/BMKMapManager.h>
 #import "AppDelegate.h"
 #import "BNCoreServices.h"
